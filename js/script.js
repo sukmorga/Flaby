3704ba8d4082df3dd9d6c70b8f8265decf3fd106
sukmorga <naginks@outlook.com>
2021-02-11 20:34:28 +0200

    add slider

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 1500,
+        modifier: 1,
+        slideShadows: false,
+    },
+    navigation: {
+        nextEl: '.btn-next',
+        prevEl: '.btn-prev',
+    },
+    spaceBetween: -550,
+});
